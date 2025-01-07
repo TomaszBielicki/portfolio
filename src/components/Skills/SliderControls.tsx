8ce61fde961fbda14198a681fc5e28060ef53b26
Tomasz Bielicki <tomek.bielicki@gmail.com>
2025-01-07 17:23:23 +0100

    slider controls buttons

---
+++ src/components/Skills/SliderControls.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Box, IconButton } from "@mui/material";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+interface SliderControlsProps {
+  onPrev: () => void;
+  onNext: () => void;
+}
+
+export const SliderControls = ({ onPrev, onNext }: SliderControlsProps) => (
+  <>
+    <IconButton
+      onClick={onPrev}
+      sx={{
+        backgroundColor: "background.paper",
+        "&:hover": { backgroundColor: "primary.main" },
+        zIndex: 2,
+      }}
+    >
+      <ChevronLeft />
+    </IconButton>
+
+    <IconButton
+      onClick={onNext}
+      sx={{
+        backgroundColor: "background.paper",
+        "&:hover": { backgroundColor: "primary.main" },
+        zIndex: 2,
+      }}
+    >
+      <ChevronRight />
+    </IconButton>
+  </>
+);
