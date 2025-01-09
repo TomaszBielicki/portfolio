354720aeead517f6035f28bdec7d44077afcb6d2
Tomasz Bielicki <tomek.bielicki@gmail.com>
2025-01-09 17:01:05 +0100

    projects data

---
+++ src/components/Projects/projectsData.ts
@@ -0,0 +1,31 @@
+import { Projects } from "./types";
+
+export const projects: Projects[] = [
+  {
+    title: "Batix",
+    description:
+      "Window Decoration Web Service based on React e-commers application with products related to window decoration, such as blinds, curtains, drapes, and accessories",
+    image:
+      "https://images.unsplash.com/photo-1460925895917-afdab827c52f?auto=format&fit=crop&w=800&q=80",
+    technologies: ["React", "NextJS", "TypeScript", "Styled Components", "CMS"],
+    live: "https://www.sklepbatix.net/",
+  },
+  {
+    title: "Beauty Salon Service",
+    description:
+      "The website Kraina Piękna is a beauty salon offering a wide range of cosmetic treatments",
+    image:
+      "https://images.unsplash.com/photo-1563986768609-322da13575f3?auto=format&fit=crop&w=800&q=80",
+    technologies: ["React", "TypeScript", "Styled Components"],
+    live: "https://www.kraina-piekna.com/",
+  },
+  {
+    title: "Wlabadi Dog Hotel and Care",
+    description:
+      "Wlabadi is a service dedicated to the care and well-being of dogs, offering various grooming services, spa treatments, and dog walking.",
+    image:
+      "https://images.unsplash.com/photo-1563986768609-322da13575f3?auto=format&fit=crop&w=800&q=80",
+    technologies: ["React", "TypeScript", "Styled Components"],
+    live: "https://www.wlabadi.com/",
+  },
+];
